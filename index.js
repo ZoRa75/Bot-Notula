b61d0a3532fb800a9f6ccc3439df017f8a66d689
ZoRa75 <100467970+ZoRa75@users.noreply.github.com>
2022-03-28 00:29:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,43 +6,28 @@ var Client = new Discord.Client({
     ]
 });
 
-const prefix = "$";
+const prefix = "/";
 
 Client.on("ready", () => {
   console.log("bot fonctionnel !")
+  Client.user.setActivity("WATHCHING Chronalll");// WATCHING, LISTENING ou pas type mais url:lien twitch pour STREAMING
+  Client.user.setStatus('online'); //dnd, invisible, online, idle
 });
 
-Client.on("messageCreate", message => {
-    if (message.authorbot) return;
-
-    //$help
-    if (message.content === prefix + "help"){
-        const embed = new Discord.MessageEmbed()
-        .setTitle("Liste des commandes")
-        .setDescription("- $ip : Voir l'ip du serveur\n - $vote : Avoir le lien vote du serveur !\n - $say : Envoyez un message via au bot (command admin)")
-    
-
-        message.channel.send({ embeds: [embed] });
-    }
-    //$ip
-    else if(message.content === prefix + "ip"){
-        const embed = new Discord.MessageEmbed()
-        .setTitle("- Ip:")
-        .setDescription("- Soon...")
-
-         message.channel.send({ embeds: [embed]});
-    }
-    //$vote
-    else if(message.content === prefix + "vote"){
-      const embed = new Discord.MessageEmbed()
-      .setTitle("- Vote:")
-      .setDescription("-  https://minecraftpocket-servers.com/server/115686/vote/")
-      .setURL(" https://minecraftpocket-servers.com/server/115686/vote/")
-
-       message.channel.send({ embeds: [embed]});
+Client.on('messageCreate', message => {
+    if (message.content.startsWith(prefix + 'say')) {
+        if (message.author.bot) return;
   
-  }
-});
+  if (!message.member.roles.cache.has('957727507111641160')) return message.delete();
+        const SayMessage = message.content.slice(4).trim();
+  
+        let embed = new Discord.MessageEmbed()
+        .setDescription(SayMessage)
+        .setColor('#671394')
+  
+        message.channel.send({ embeds: [embed] }) 
+    }
+  });
 
 
 Client.login(process.env.BOT_TOKEN)
