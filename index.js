cdebcd4c513d6ddb1186df055548307c5b878dfa
ZoRa75 <100467970+ZoRa75@users.noreply.github.com>
2022-04-08 21:44:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,28 +6,27 @@ var Client = new Discord.Client({
     ]
 });
 
-const prefix = "/";
+const prefix = "$";
 
 Client.on("ready", () => {
   console.log("bot fonctionnel !")
-  Client.user.setActivity("WATHCHING Chronalll");// WATCHING, LISTENING ou pas type mais url:lien twitch pour STREAMING
+  Client.user.setActivity("NotulaKitmap");// WATCHING, LISTENING ou pas type mais url:lien twitch pour STREAMING  
   Client.user.setStatus('online'); //dnd, invisible, online, idle
 });
 
-Client.on('messageCreate', message => {
-    if (message.content.startsWith(prefix + 'say')) {
-        if (message.author.bot) return;
-  
-  if (!message.member.roles.cache.has('957727507111641160')) return message.delete();
-        const SayMessage = message.content.slice(4).trim();
-  
-        let embed = new Discord.MessageEmbed()
-        .setDescription(SayMessage)
-        .setColor('#671394')
-  
-        message.channel.send({ embeds: [embed] }) 
-    }
-  });
+Client.on('message', message => {
+if (message.content.toLowerCase().startsWith("say")) 
+ {
+  let MSG = message.content.split(" ");
+  let Query = MSG.slice(1).join("+");
+  let QueryD = MSG.slice(1).join(" ");
+  if (!Query) message.reply("Please specify something for me to say!")
+  else
+  {
+    message.channel.send(QueryD + " -" + message.author.tag)
+}
+}
 
 
 Client.login(process.env.BOT_TOKEN);
+});
